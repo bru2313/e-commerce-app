b7797be82aee53bf25af7f3d60a615ae8aa65585
bru2313 <bru@adaptiveresponse.net>
2020-11-08 16:51:28 +0000

    adding header component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import React from'react';
 import './App.css';
 import HomePage from './pages/homepage/homepage.component';
-import ShopPage from './pages/shop/shop-component';
+import ShopPage from './pages/shop/shop.component';
+import Header from './components/header/header-component';
 
 
 import { Route, Switch } from 'react-router-dom';
@@ -9,6 +10,7 @@ import { Route, Switch } from 'react-router-dom';
 function App() {
   return (
     <div>
+      <Header />
       <Switch>
         <Route exact path='/' component={HomePage} />
         <Route path='/shop' component={ShopPage} />
