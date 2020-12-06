08e48fc0913db02f3a8f05d40102981b7b3ee40b
bru2313 <bru@adaptiveresponse.net>
2020-12-06 16:40:44 +0000

    multiple cart items

--- src/components/collection-item/collection-item.component.jsx
+++ src/components/collection-item/collection-item.component.jsx
@@ -1,23 +1,35 @@
 import React from 'react';
+import { connect } from 'react-redux';
+
 import Layout from '../layout';
+import CustomButton from '../custom-button/custom-button.component';
+import { addItem } from '../../redux/cart/cart.actions';
 import './collection-item.styles.scss';
 
-const CollectionItem = ({ id, name, price, imageUrl }) => (
-    <Layout>
-    <div className='collection-item'>
-        <div 
-            className='image'
-            style={{
-                backgroundImage: `url(${imageUrl})`
-            }}
-        >
-        </div>
-        <div className='collection-footer'>
-            <span className='name'>{name}</span>
-            <span className='price'>{price}</span> 
-        </div> 
-    </div>
-    </Layout>
-);
+const CollectionItem = ({ item, addItem }) => {
+    const { name, price, imageUrl } = item;
+    return (
+        <Layout>
+            <div className='collection-item'>
+                <div 
+                    className='image'
+                    style={{
+                        backgroundImage: `url(${imageUrl})`
+                    }}
+                >
+                </div>
+                <div className='collection-footer'>
+                    <span className='name'>{name}</span>
+                    <span className='price'>{price}</span> 
+                </div> 
+                <CustomButton onClick={() => addItem(item)} inverted> Add to Cart </CustomButton>
+            </div>
+        </Layout>
+    )
+};
 
-export default CollectionItem;
+const mapDispatchToProps = dispatch => ({
+    addItem: item => dispatch(addItem(item))
+})
+
+export default connect(null, mapDispatchToProps)(CollectionItem);
