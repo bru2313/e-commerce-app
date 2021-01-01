4703e4b41d926e0ee715fc04e5de5c1e8a556f91
bru2313 <bru@adaptiveresponse.net>
2021-01-01 09:43:15 +0000

    shp data from firestore

--- src/firebase/firebase-utils.js
+++ src/firebase/firebase-utils.js
@@ -48,7 +48,6 @@ const config = {
 
   export const addCollectionAndDocuments = async (collectionKey, objectsToAdd) => {
     const collectionRef = firestore.collection(collectionKey);
-    console.log(collectionRef);
     const batch = firestore.batch();
     objectsToAdd.forEach(obj => {
       const newDocRef = collectionRef.doc();
@@ -58,6 +57,23 @@ const config = {
     return await batch.commit();
   };
   
+  export const convertCollectionSnapshotToMap = (collections) => {
+    const transformedCollection = collections.docs.map(doc => {
+      const { title, items } = doc.data();
+
+      return {
+        routeName: encodeURI(title.toLowerCase()),
+        id: doc.id,
+        title,
+        items
+      };
+    });
+
+    return transformedCollection.reduce((accumulator, collection) =>{
+      accumulator[collection.title.toLowerCase()] = collection;
+      return accumulator;
+    } , {});
+  };
 
   firebase.initializeApp(config);
 
